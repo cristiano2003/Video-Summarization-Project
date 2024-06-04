7c7122e770576c29397e433b17cb5e1120305297
Trung Trực Nguyễn <trungtrucnguyen@192.168.0.100>
2024-06-04 11:13:07 +0700

    initial commit

---
+++ src/train.py
@@ -0,0 +1,76 @@
+import logging
+from pathlib import Path
+
+from anchor_based.train import train as train_anchor_based
+from anchor_free.train import train as train_anchor_free
+from helpers import init_helper, data_helper
+import torch
+
+logger = logging.getLogger()
+import wandb
+import numpy as np
+TRAINER = {
+    'anchor-based': train_anchor_based,
+    'anchor-free': train_anchor_free
+}
+
+wandb.login(key='53f5746150b2ce7b0552996cb6acc3beec6e487f')
+wandb_log = wandb.init( project="Video Summarization")
+
+def get_trainer(model_type):
+    assert model_type in TRAINER
+    return TRAINER[model_type]
+
+
+def main():
+    args = init_helper.get_arguments()
+
+    init_helper.init_logger(args.model_dir, args.log_file)
+    init_helper.set_random_seed(args.seed)
+
+    logger.info(vars(args))
+
+    model_dir = Path(args.model_dir)
+    model_dir.mkdir(parents=True, exist_ok=True)
+    data_helper.get_ckpt_dir(model_dir).mkdir(parents=True, exist_ok=True)
+
+    trainer = get_trainer(args.model)
+
+    data_helper.dump_yaml(vars(args), model_dir / 'args.yml')
+
+    for split_path in args.splits:
+        split_path = Path(split_path)
+        splits = data_helper.load_yaml(split_path)
+
+        results = {}
+        stats = data_helper.AverageMeter('fscore')
+
+        for split_idx, split in enumerate(splits):
+            logger.info(f'Start training on {split_path.stem}: split {split_idx}')
+            ckpt_path = data_helper.get_ckpt_path(model_dir, split_path, split_idx)
+            fscore = trainer(args, split, ckpt_path)
+            stats.update(fscore=fscore)
+            results[f'split{split_idx}'] = float(fscore)
+
+        results['mean'] = float(stats.fscore)
+        data_helper.dump_yaml(results, model_dir / f'{split_path.stem}.yml')
+
+        logger.log(f'Training done on {split_path.stem}. F-score: {stats.fscore:.4f}')
+        wandb_log.log({f"{split_path.stem}-F-score":stats.fscore})
+
+if __name__ == '__main__':
+    # main()
+    split_path = Path('../splits/tvsum.yml')
+    splits = data_helper.load_yaml(split_path)
+    
+    split = splits[0]
+    train_set = data_helper.VideoDataset(split['train_keys'])
+    # print(train_set[10])
+    train_loader = data_helper.DataLoader(train_set, shuffle=True) 
+    n = 0
+    for _, seq, gtscore, cps, n_frames, nfps, picks, _ in train_loader:
+        seq = torch.tensor(seq, dtype=torch.float32)
+        print(seq[0].shape)
+        n += 1  
+      
+        break
