31fc434a9e0d52d84d2074181f71f81854c3f892
Trung Trực Nguyễn <trungtrucnguyen@192.168.0.101>
2024-06-12 22:54:33 +0700

    fix

--- inference.py
+++ inference.py
@@ -1,14 +1,64 @@
 import cv2
 import numpy as np
 import torch
-
+import argparse
 from src.helpers import init_helper, vsumm_helper, bbox_helper, video_helper
 from src.modules.model_zoo import get_model
 
 
-def main():
-    args = init_helper.get_arguments()
+def get_parser() -> argparse.ArgumentParser:
+    parser = argparse.ArgumentParser()
 
+    # model type
+    parser.add_argument('model', type=str,
+                        choices=('anchor-based', 'anchor-free'))
+    parser.add_argument('--dataset', type=str, default='tvsum')
+    # training & evaluation
+    parser.add_argument('--device', type=str, default='cuda',
+                        choices=('cuda', 'cpu'))
+    parser.add_argument('--seed', type=int, default=12345)
+    parser.add_argument('--splits', type=str, nargs='+', default=[])
+    parser.add_argument('--max-epoch', type=int, default=300)
+    parser.add_argument('--model-dir', type=str, default='../models/model')
+    parser.add_argument('--log-file', type=str, default='log.txt')
+    parser.add_argument('--lr', type=float, default=5e-5)
+    parser.add_argument('--weight-decay', type=float, default=1e-5)
+    parser.add_argument('--lambda-reg', type=float, default=1.0)
+    parser.add_argument('--nms-thresh', type=float, default=0.5)
+
+    # inference
+    parser.add_argument('--ckpt-path', type=str, default=None)
+    parser.add_argument('--sample-rate', type=int, default=15)
+    parser.add_argument('--source', type=str, default=None)
+    parser.add_argument('--save-path', type=str, default=None)
+
+    parser.add_argument('--num-head', type=int, default=8)
+    parser.add_argument('--num-feature', type=int, default=1024)
+    parser.add_argument('--num-hidden', type=int, default=128)
+
+    # anchor based
+    parser.add_argument('--neg-sample-ratio', type=float, default=2.0)
+    parser.add_argument('--incomplete-sample-ratio', type=float, default=1.0)
+    parser.add_argument('--pos-iou-thresh', type=float, default=0.6)
+    parser.add_argument('--neg-iou-thresh', type=float, default=0.0)
+    parser.add_argument('--incomplete-iou-thresh', type=float, default=0.3)
+    parser.add_argument('--anchor-scales', type=int, nargs='+',
+                        default=[4, 8, 16, 32])
+
+    # anchor free
+    parser.add_argument('--lambda-ctr', type=float, default=1.0)
+    parser.add_argument('--cls-loss', type=str, default='focal',
+                        choices=['focal', 'cross-entropy'])
+    parser.add_argument('--reg-loss', type=str, default='soft-iou',
+                        choices=['soft-iou', 'smooth-l1'])
+
+    return parser
+
+
+def main():
+    parser  =  get_parser()
+    args = parser.parse_args()
+    
     # load model
     print('Loading DSNet model ...')
     model = get_model(args.model, **vars(args))
